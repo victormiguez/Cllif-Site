c36006e8ab76ed3238ea2428dfc4c8f28404a4a4
Julio <juliohisloti@gmail.com>
2013-08-30 20:20:11 -0300

    Subindo site

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+//Cllif JS
+	var height = document.documentElement.clientHeight;
+	//Colocar valor no elento
+	if(height > 600){
+
+		$('.page').show().css({'height':height + "px"});	
+
+	}else{
+		//Nada
+	}
+	
